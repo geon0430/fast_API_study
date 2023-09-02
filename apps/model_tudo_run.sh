a80d000671e29da5dc8513532546aaac74393188
geon0430 <jack058725@naver.com>
2023-09-02 10:43:36 +0000

    스웨거 실행

--- apps/model_tudo_run.sh
+++ apps/model_tudo_run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash/
-curl -X GET  http://192.168.10.10:10002/tudo/1  -H 'accept: application/json' 
-#-H 'Content-Type: application/json' -d '{"id": 1, "item": "SomeName"}'
+curl -X GET  http://192.168.10.10:10002/tudo/1  -H 'accept: application/json' -H 'Content-Type: application/json' -d '{"id": 1, "item": "SomeName"}'
