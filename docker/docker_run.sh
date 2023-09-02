9accef1b3145400b2ba4a8f859793fb7dbb596a2
geon <jack058725@naver.com>
2023-09-02 14:33:08 +0900

    dockerfile 수정

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,12 +1,14 @@
-BASE_PATH=$(dirname $(pwd))
+#!/bin/bash
 
 docker run \
-	-it
-	--name geon-web \
-	-p 10001:8888 \
- 	-p 10002:8000 \
- 	-p 10003:8080 \
- 	-v /ldap/home/geon:/home \
-	-v -v $BASE_PATH:/workspace \
-	geon-web
+  --gpus all \
+  --restart always \
+  --shm-size=2g \
+  -it \
+  --name geon-fastAPI_study \
+  -p 40001:8888 \
+  -p 40002:8000 \
+  -p 40003:8080 \
+  -v /home/geon/Documents/docker_data/fast_API_study:/home/fast_API_study \
+  fast_api:latest
 
