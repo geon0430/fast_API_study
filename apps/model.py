a4bf53c3579f2a3f860ad9fb0a58ede01299ba8b
geon0430 <jack058725@naver.com>
2023-09-03 13:40:49 +0000

    chapter 4 clear

--- apps/model.py
+++ apps/model.py
@@ -1,40 +1,54 @@
+from typing import List, Optional
+
+from fastapi import Form
 from pydantic import BaseModel
-from typing import List
 
-class Tudo(BaseModel):
-    id : int
-    item : str
 
-    class Config:
-        schema_extra = {
-                "example" : {
-                    "id": 1,
-                    "item": "Example Schema!"
-                    }
-        }
-class Tudoitem(BaseModel):
-    item : str
+class Todo(BaseModel):
+    id: Optional[int]
+    item: str
 
-    class Config:
-        shema_extra = {
-                "example" : {
-                    "item" : "read the next chapter of the book"
-                    }
-    }
-class Tudoitems(BaseModel):
-    tudos : List[Tudoitem]
+    @classmethod
+    def as_form(
+        cls,
+        item: str = Form(...)
+    ):
+        return cls(item=item)
+
 
     class Config:
         schema_extra = {
-                "example":{
-                    "tudos": [
-                        {
-                            "item": "example schema 1!"
-                        },
-                        {
-                            "item": "example schema 2!"
-                        }
-                    ]
-                }
+            "example": {
+                "id": 1,
+                "item": "Example schema!"
             }
+        }
 
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
+            }
+        }
